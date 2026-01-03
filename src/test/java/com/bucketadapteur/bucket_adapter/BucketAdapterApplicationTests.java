bbfc51ae7d789fc7ed031de66475a5b696019916
T5uy0 <julienschneider964@gmail.com>
2026-01-03 14:22:38 +0100

    initial commit

---
+++ src/test/java/com/bucketadapteur/bucket_adapter/BucketAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bucketadapteur.bucket_adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
