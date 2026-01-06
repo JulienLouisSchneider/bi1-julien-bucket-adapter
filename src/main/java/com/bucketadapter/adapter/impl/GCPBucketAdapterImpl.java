2833b9011d358dd7ecfce6bf800a2830f59ad592
T5uy0 <julienschneider964@gmail.com>
2026-01-06 13:54:59 +0100

    feat: add upload on gcp adapter.

--- src/main/java/com/bucketadapter/adapter/impl/GCPBucketAdapterImpl.java
+++ src/main/java/com/bucketadapter/adapter/impl/GCPBucketAdapterImpl.java
@@ -1,26 +1,53 @@
 package com.bucketadapter.adapter.impl;
 
 import com.bucketadapter.adapter.BucketAdapter;
+import com.bucketadapter.bucketadapterexceptions.BucketObjectNotFoundException;
+import com.bucketadapter.bucketadapterexceptions.BucketOperationException;
+import com.bucketadapter.bucketadapterexceptions.InvalidBucketPathException;
+import com.bucketadapter.helpers.AdapterHelper;
+import com.google.api.gax.paging.Page;
+import com.google.cloud.storage.*;
 import org.springframework.stereotype.Component;
+import com.bucketadapter.config.GcpStorageConfig;
 
-import java.util.List;
+import java.util.*;
 
 @Component("GCP")
 public class GCPBucketAdapterImpl implements BucketAdapter {
-    @Override
-    public void upload(String remote, byte[] object) {
 
+  private final Storage storage;
+
+  public GCPBucketAdapterImpl(Storage storage) {
+    this.storage = storage;
+  }
+
+  @Override
+  public void upload(String remote, byte[] object) {
+    AdapterHelper.requirePayload(object); // null check :contentReference[oaicite:2]{index=2}
+    var ref =
+        AdapterHelper.requireObjectKey(
+            AdapterHelper.parseRemote(
+                remote)); // interdit "" et "/" :contentReference[oaicite:3]{index=3}
+
+    try {
+      BlobId id = BlobId.of(ref.bucket(), ref.keyOrPrefix());
+      BlobInfo info = BlobInfo.newBuilder(id).build();
+      storage.create(info, object); // overwrite par défaut
+
+    } catch (StorageException e) {
+      throw AdapterHelper.mapGcsException(e);
+    } catch (RuntimeException e) {
+      throw new BucketOperationException("Operation failed.", e);
     }
+  }
 
-    @Override
-    public byte[] download(String remote) {
-        return new byte[0];
-    }
+  @Override
+  public byte[] download(String remote) {
+    return new byte[0];
+  }
 
-    @Override
-    public void delete(String remote, boolean recursive) {
-
-    }
+  @Override
+  public void delete(String remote, boolean recursive) {}
 
     @Override
     public List<String> list(String remote, boolean recursive) {
