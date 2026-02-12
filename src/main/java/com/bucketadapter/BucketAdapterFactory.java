9ca0cdf2ada3116dc97feec5d1ed4fa01801a2c0
T5uy0 <julienschneider964@gmail.com>
2026-02-12 11:33:02 +0100

    refactor: constructor injection

--- src/main/java/com/bucketadapter/BucketAdapterFactory.java
+++ src/main/java/com/bucketadapter/BucketAdapterFactory.java
@@ -1,7 +1,7 @@
 package com.bucketadapter;
 
 import com.bucketadapter.adapter.BucketAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -9,36 +9,21 @@ import java.util.Map;
 @Component
 public class BucketAdapterFactory {
 
-  //TODO NGY Avoid using field injection -> method injection
-  @Autowired private Map<String, BucketAdapter> adapters;
+  private final Map<String, BucketAdapter> adapters;
+  private final String provider;
+
+  public BucketAdapterFactory(
+      Map<String, BucketAdapter> adapters, @Value("${PROVIDER_IMPL}") String provider) {
+    this.adapters = adapters;
+    this.provider = provider;
+  }
 
   public BucketAdapter getAdapter() {
-
-    String provider = getConfig();
-
     BucketAdapter adapter = adapters.get(provider);
     if (adapter == null) {
       throw new IllegalArgumentException(
           "Unsupported provider: " + provider + ". Available: " + adapters.keySet());
     }
     return adapter;
-  }
-
-  private static String getConfig() {
-
-    String value = System.getProperty("PROVIDER_IMPL");
-
-    if (value == null || value.isBlank()) {
-      throw new IllegalStateException(
-          """
-                      Type of provider\
-                       is not configured.
-                      When running locally: Add to .env file as \
-                      PROVIDER_IMPL\
-                      =value
-                      When running in Docker: Set environment variable \
-                      PROVIDER_IMPL""");
-    }
-    return value;
   }
 }
